ac856f41f1c5d2bfd68eca2c3d157287d8abfcda
codewithsaidul <codewithsaidul@gmail.com>
2024-03-26 00:53:05 +0600

    Bookcard Componet Data Added From API or JSON

--- src/componets/BookCard.jsx
+++ src/componets/BookCard.jsx
@@ -2,37 +2,41 @@ import PropTypes from 'prop-types'
 import { CiStar } from "react-icons/ci";
 
 const BookCard = ({book}) => {
-  console.log(book)
+  const {bookName, author, image, tags, category, rating  } = book;
   return (
     <div className="card border">
-      <figure className="p-5 m-5 rounded-lg bg-[#F3F3F3]">
-        <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="book"
-          className="rounded-xl"
-        />
+      <figure className="p-5 m-5 rounded-lg h-[300px]  bg-[#F3F3F3]">
+        <img src={image} alt="book" className="rounded-xl h-full" />
       </figure>
 
-      <div className="py-3 px-5">
+      <div className="py-5 px-5">
         <div className="flex gap-3 mb-5">
-          <h3 className="text-base font-medium text-[#23BE0A] py-2 px-4 bg-[#23BE0A0D] rounded-[30px]">
+          {/* <h3 className="text-base font-medium text-[#23BE0A] py-2 px-4 bg-[#23BE0A0D] rounded-[30px]">
             Young Adult
-          </h3>
-          <h3 className="text-base font-medium text-[#23BE0A] py-2 px-4 bg-[#23BE0A0D] rounded-[30px]">
+          </h3> */}
+
+          {tags.map((tag) => (
+            <h3
+              key={tag}
+              className="text-base font-medium text-[#23BE0A] py-2 px-4 bg-[#23BE0A0D] rounded-[30px]"
+            >
+        
+              {tag}
+            </h3>
+          ))}
+          {/* <h3 className="text-base font-medium text-[#23BE0A] py-2 px-4 bg-[#23BE0A0D] rounded-[30px]">
             Identity
-          </h3>
+          </h3> */}
         </div>
 
-        <h2 className="text-2xl text-[#131313] font-bold mb-3">
-          The Catcher in the Rye
-        </h2>
+        <h2 className="text-2xl text-[#131313] font-bold mb-3">{bookName}</h2>
         <h4 className="text-base font-medium text-[#131313CC]">
-          By : Awlad Hossain
+          By : {author}
         </h4>
         <div className="flex justify-between items-center border-t border-[#13131326] pt-5 mt-5 border-dashed">
-          <h5 className="text-base font-medium text-[#131313CC]">Fiction</h5>
+          <h5 className="text-base font-medium text-[#131313CC]">{category}</h5>
           <h5 className="text-base flex items-center font-medium text-[#131313CC]">
-            5.00 <CiStar size={20} className='ml-1' />
+            {rating} <CiStar size={20} className="ml-1" />
           </h5>
         </div>
       </div>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -16,7 +16,7 @@ const Books = () => {
       <h2 className="text-5xl text-center font-bold text-[#131313] mt-20 mb-10">Books</h2> 
 
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-10">
+      <div className="grid grid-cols-1 px-4 md:grid-cols-2 lg:grid-cols-3 gap-x-7 gap-y-12 mb-10">
             {
                 books.map(book => <BookCard key={book.bookId} book={book}></BookCard>)
             }
